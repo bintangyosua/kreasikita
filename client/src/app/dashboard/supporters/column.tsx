734b7f486dfb3ebff4e7444629b35fdf87aa505c
Bintang <itzbintangyosua@gmail.com>
2024-05-24 21:40:00 +0700

    added dashboard supporters

---
+++ client/src/app/dashboard/supporters/column.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal, ArrowUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export type Payment = {
+  id: number;
+  amount: number;
+  name: string;
+  message: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+          Nama
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "amount",
+    header: ({ column }) => {
+      return (
+        <div className="w-full flex justify-end">
+          <Button
+            variant="ghost"
+            className="text-right"
+            onClick={() =>
+              column.toggleSorting(column.getIsSorted() === "asc")
+            }>
+            Amount
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        </div>
+      );
+    },
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "message",
+    header: "Message",
+  },
+];
--- client/src/app/dashboard/supporters/page.tsx
+++ client/src/app/dashboard/supporters/page.tsx
@@ -1,13 +1,54 @@
 import Layout from "@/components/dashboard/layout";
 import Section from "@/components/dashboard/section";
 import React from "react";
+import { DataTable } from "./data-table";
+import { columns } from "./column";
+
+type Donation = {
+  id: number;
+  amount: number;
+  name: string;
+  message: string;
+};
+
+const donations: Donation[] = [
+  {
+    id: 1,
+    name: "John Doe",
+    amount: 10000,
+    message: "Kak",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    amount: 24000,
+    message: "Apa kabar?",
+  },
+];
 
 export default function Page() {
   return (
     <Layout page="supporters">
+      {/* Statistics */}
       <Section>
-        <div className="w-full"></div>
+        <div className="w-full flex flex-col gap-3 sm:flex-row">
+          <CardLayout name="Supporters" num={"345"} />
+          <CardLayout name="Last 30 days" num={"Rp345.000,-"} />
+          <CardLayout name="All-time" num={"Rp412.000,-"} />
+        </div>
+      </Section>
+      <Section>
+        <DataTable columns={columns} data={donations}></DataTable>
       </Section>
     </Layout>
+  );
+}
+
+function CardLayout({ num, name }: { num: string; name: string }) {
+  return (
+    <div className="border sm:w-1/3 flex flex-col gap-1 border-gray-500 px-3 py-3 rounded-xl w-full">
+      <h2 className="text-2xl font-bold">{num}</h2>
+      <span className="">{name}</span>
+    </div>
   );
 }
