b69929b41b60e43df82eef0066f9b47fbf5000d9
ZiaKfa <zia14148@gmail.com>
2024-05-24 14:54:56 +0700

    menambahkan payment

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,9 +8,10 @@ import { CategoryModule } from './category/category.module';
 import { DonationModule } from './donation/donation.module';
 import { PayoutModule } from './payout/payout.module';
 import { MilestoneModule } from './milestone/milestone.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, PrismaModule, CategoryModule, DonationModule, PayoutModule, MilestoneModule],
+  imports: [UsersModule, AuthModule, PrismaModule, CategoryModule, DonationModule, PayoutModule, MilestoneModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
