744bd436b930bf5f90396bac7537aa3a327baf8d
Bintang <itzbintangyosua@gmail.com>
2024-06-01 09:08:14 +0700

    supporters data table with primereact

--- client/src/app/dashboard/supporters/page.tsx
+++ client/src/app/dashboard/supporters/page.tsx
@@ -1,12 +1,14 @@
 import Layout from "@/components/dashboard/layout";
 import Section from "@/components/dashboard/section";
 import React from "react";
-import { DataTable } from "./data-table";
-import { columns } from "./column";
+// import { DataTable } from "./data-table";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
 type Donation = {
   id: number;
   amount: number;
+  email: string;
   name: string;
   message: string;
 };
@@ -14,13 +16,15 @@ type Donation = {
 const donations: Donation[] = [
   {
     id: 1,
-    name: "John Doe",
+    name: "Danjin",
+    email: "danjin@gg.com",
     amount: 10000,
     message: "Kak",
   },
   {
     id: 2,
-    name: "Jane Doe",
+    name: "Josephine",
+    email: "josephind@outlook.com",
     amount: 24000,
     message: "Apa kabar?",
   },
@@ -29,16 +33,34 @@ const donations: Donation[] = [
 export default function Page() {
   return (
     <Layout page="supporters">
+      <h1 className="text-5xl font-bold w-full lg:w-2/3 xl:1/2 mx-auto mb-3">
+        Supporters
+      </h1>
+
       {/* Statistics */}
       <Section>
-        <div className="w-full flex flex-col gap-3 sm:flex-row">
-          <CardLayout name="Supporters" num={"345"} />
-          <CardLayout name="Last 30 days" num={"Rp345.000,-"} />
-          <CardLayout name="All-time" num={"Rp412.000,-"} />
+        <div className="p-5">
+          <div className="w-full flex flex-col gap-3 sm:flex-row mb-4">
+            <CardLayout name="Supporters" num={"345"} />
+            <CardLayout name="Last 30 days" num={"Rp345.000,-"} />
+            <CardLayout name="All-time" num={"Rp412.000,-"} />
+          </div>
+
+          {/* Table */}
+          <DataTable
+            size="small"
+            value={donations}
+            paginator
+            rows={5}
+            rowsPerPageOptions={[5, 10, 25, 50]}
+            tableStyle={{ minWidth: "50rem" }}>
+            <Column field="id" header="ID"></Column>
+            <Column field="name" header="Nama"></Column>
+            <Column field="email" header="Email"></Column>
+            <Column field="amount" header="Total Donasi"></Column>
+          </DataTable>
         </div>
-      </Section>
-      <Section>
-        <DataTable columns={columns} data={donations}></DataTable>
+        {/* <DataTable columns={columns} data={donations}></DataTable> */}
       </Section>
     </Layout>
   );
@@ -46,7 +68,7 @@ export default function Page() {
 
 function CardLayout({ num, name }: { num: string; name: string }) {
   return (
-    <div className="border sm:w-1/3 flex flex-col gap-1 border-gray-500 px-3 py-3 rounded-xl w-full">
+    <div className="sm:w-1/3 flex flex-col gap-1 px-3 py-3 rounded-xl w-full">
       <h2 className="text-2xl font-bold">{num}</h2>
       <span className="">{name}</span>
     </div>
