9e6ede27f7448146c545cbffc0d361bbc171b1eb
Bintang <itzbintangyosua@gmail.com>
2024-06-14 23:19:22 +0700

    deploy

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.inflact.com"],
+    domains: ["cdn.inflact.com", "kreasikita.rplinformatika.my.id"],
   },
   experimental: {
     serverActions: {
